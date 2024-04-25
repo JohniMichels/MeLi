ac469a12f599e303bccbabde76043bc8219d3a27
Johni Michels <johni.michels@gmail.com>
2024-04-25 08:13:30 -0300

    Refactor table names in trigger_example.sql to use plural form

--- Challenge Engineer - Primera Parte - SQL/trigger_example.sql
+++ Challenge Engineer - Primera Parte - SQL/trigger_example.sql
@@ -1,6 +1,6 @@
 -- usando eventsourcing para fazer um cdc de tabela
 
-CREATE TABLE Item (
+CREATE TABLE Items (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     seller_id UUID NOT NULL,
     name VARCHAR(60) NOT NULL, -- Procurei e parece que o limite no Mercado Livre é de 60 caracteres
@@ -9,8 +9,8 @@ CREATE TABLE Item (
     price DECIMAL(10, 2) NOT NULL, -- Usando DECIMAL para representar valores monetários, limite de 10 dígitos sendo 2 deles decimais. Isso significa que o valor máximo que podemos representar é 100 milhões
     status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE' CHECK (status IN ('ACTIVE', 'INACTIVE', 'CANCELED')),
     cancel_date TIMESTAMP DEFAULT NULL,
-    FOREIGN KEY (category_id) REFERENCES Category(id),
-    FOREIGN KEY (seller_id) REFERENCES Customer(id)
+    FOREIGN KEY (category_id) REFERENCES Categories(id),
+    FOREIGN KEY (seller_id) REFERENCES Customers(id)
 );
 
 CREATE TABLE ItemCDC (
@@ -26,22 +26,27 @@ CREATE TABLE ItemCDC (
     status VARCHAR(10) NOT NULL CHECK (status IN ('ACTIVE', 'INACTIVE', 'CANCELED')),
     cancel_date TIMESTAMP,
     FOREIGN KEY (category_id) REFERENCES Categories(id),
-    FOREIGN KEY (seller_id) REFERENCES Categories(id),
+    FOREIGN KEY (seller_id) REFERENCES Customers(id),
     FOREIGN KEY (id) REFERENCES Items(id)
 );
 
-CREATE FUNCTION NotifyItemChanged()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION NotifyItemChanged()
+RETURNS TRIGGER AS $itemcdcnotification$
 BEGIN
-    IF (TG_OP = 'INSERT') OR (TG_OP = 'UPDATE') THEN
-        INSERT INTO ItemCDC (update_type, id, seller_id, name, description, category_id, price, status, cancel_date)
-        VALUES (TG_OP, NEW.id, NEW.seller_id, NEW.name, NEW.description, NEW.category_id, NEW.price, NEW.status, NEW.cancel_date);
-    ELSIF (TG_OP = 'DELETE') THEN
-        INSERT INTO ItemCDC (update_type, id, seller_id, name, description, category_id, price, status, cancel_date)
-        VALUES ('DELETE', OLD.id, OLD.seller_id, OLD.name, OLD.description, OLD.category_id, OLD.price, OLD.status, OLD.cancel_date);
+    IF TG_OP = 'DELETE' THEN
+        INSERT INTO itemcdc (
+            update_type, id, seller_id, name, description, category_id, price, status, cancel_date
+        ) 
+        SELECT TG_OP, OLD.*;
+    ELSE
+        INSERT INTO itemcdc (
+            update_type, id, seller_id, name, description, category_id, price, status, cancel_date
+        )
+        SELECT TG_OP, NEW.*;
     END IF;
-    RETURN NEW;
+    RETURN NULL;
 END;
+$itemcdcnotification$ LANGUAGE plpgsql;
 
 CREATE TRIGGER ItemCDCTrigger
 AFTER INSERT OR UPDATE OR DELETE
